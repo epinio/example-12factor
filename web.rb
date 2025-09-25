51624c32de8251249ed76abe66c3076cc499bdcb
David Johnson <dcharles525@gmail.com>
2025-09-25 11:51:47 -0400

    Update web.rb

--- web.rb
+++ web.rb
@@ -5,7 +5,7 @@ require 'rack/ssl-enforcer'
 
 configure do
   # For Development use only!
-  use Rack::Protection, except: [:host_authorization]
+  disable :protection
   use Rack::SslEnforcer if ENV['FORCE_SSL']
   I18n.enforce_available_locales = true
   I18n.load_path += Dir[File.join(settings.root, 'locales', '*.yml')]
