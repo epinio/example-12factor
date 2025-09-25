951ca9bfe52bdc8e0ff578d81420705f3f70d8e5
David Johnson <dcharles525@gmail.com>
2025-09-25 11:17:28 -0400

    Update web.rb
    Currently, the app deploys but you can't view the app due to sinatara restrictions. Since this is a demo app, and the host will be different for each Epinio, deployment, we will disable host checking

--- web.rb
+++ web.rb
@@ -3,6 +3,9 @@ require 'maruku'
 require 'i18n'
 require 'rack/ssl-enforcer'
 
+# For Development use only!
+set :protection, except: :host_authorization
+
 configure do
   use Rack::SslEnforcer if ENV['FORCE_SSL']
   I18n.enforce_available_locales = true
